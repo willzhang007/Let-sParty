67d13952482c7652f22ae490c36466d492493b88
Lichao Zhang <lichaozhang007@gmail.com>
2018-04-12 00:23:29 -0400

    initial commit

--- Desktop/Client/Pods/Target Support Files/Pods-LETSPARTYTests/Pods-LETSPARTYTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LETSPARTYTests : NSObject
-@end
-@implementation PodsDummy_Pods_LETSPARTYTests
-@end
--- Desktop/Client/Pods/Target Support Files/Pods-LETSPARTYUITests/Pods-LETSPARTYUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LETSPARTYUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LETSPARTYUITests
-@end
